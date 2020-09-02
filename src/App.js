eb2a4acc2f3bdcd132d4944a2dd3b4f2e784c914
dean <deanlloyd64@gmail.com>
2020-09-02 17:23:33 -0400

    import createStore, applymiddleWare, and compose for redux

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import ReactDom from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
 
 function App() {
   return (
