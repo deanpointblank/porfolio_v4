117ad2fe6794c5ec50a87338e835ca3d616550f7
dean <deanlloyd64@gmail.com>
2020-09-02 17:27:20 -0400

    import redux thunk to project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ReactDom from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 
 
