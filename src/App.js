138b6ebfcbf7544d33be699dec173ee1c6f6a00b
dean <deanlloyd64@gmail.com>
2020-09-02 18:04:48 -0400

    added basic router components, removed default styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import ReactDom from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -10,6 +8,7 @@ import thunk from 'redux-thunk';
 
 import { allReducers } from './Reducers/index'
 
+
 const store = createStore(allReducers, compose(
   spplyMiddleware(thunk),
   windoww.devToolExtension ? window.devToolExtension() : f => f
@@ -17,22 +16,11 @@ const store = createStore(allReducers, compose(
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+
+      </Router>
+    </Provider>
   );
 }
 
