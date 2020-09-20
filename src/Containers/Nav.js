247b93e8682ba90fbd3b34ee6f664f89617b5bea
dean <deanlloyd64@gmail.com>
2020-09-20 18:09:02 -0400

    align navbar items to the right

--- src/Containers/Nav.js
+++ src/Containers/Nav.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Col, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { GrLinkedin, GrGithub, GrTwitter, GrMail } from 'react-icons/gr'
 
 
@@ -10,14 +10,14 @@ export default class Menu extends Component {
                 <Navbar bg="light" expand="lg" fluid>
                 <Navbar.Brand href="/">Dean Betty</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                     <Nav className="">
-                        <Nav.Link href="/about">About</Nav.Link>
-                        <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-                        <Nav.Link href="/blog">Blog</Nav.Link>
-                        <Navbar.Text className="d-flex flex-row">
-                        <a className="mr-2" href="https://github.com/deanpointblank"><GrGithub /></a> <a className="mr-2 ml-2" href="https://www.linkedin.com/in/deanbetty/"><GrLinkedin /></a> <a className="mr-2 ml-2" href="https://twitter.com/TheDomDonDada"><GrTwitter /></a><a className="mr-2 ml-2" href="#Email"><GrMail /></a>
-                        </Navbar.Text>
+                            <Nav.Link href="/about">About</Nav.Link>
+                            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+                            <Nav.Link href="/blog">Blog</Nav.Link>
+                            <Navbar.Text >
+                            <a className="mr-2" href="https://github.com/deanpointblank"><GrGithub /></a> <a className="mr-2 ml-2" href="https://www.linkedin.com/in/deanbetty/"><GrLinkedin /></a> <a className="mr-2 ml-2" href="https://twitter.com/TheDomDonDada"><GrTwitter /></a><a className="mr-2 ml-2" href="#Email"><GrMail /></a>
+                            </Navbar.Text>
                     </Nav>
                 </Navbar.Collapse>
                 </Navbar>
