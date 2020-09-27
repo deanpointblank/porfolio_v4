004351653fbb3b9b8e4346f9987b202178f8abc6
dean <deanlloyd64@gmail.com>
2020-09-26 23:47:08 -0400

    write loading, add, and error cases for projects reducer

---
+++ src/Reducers/ProjectReducer.js
@@ -0,0 +1,24 @@
+const ProjectReducer = (state = {projects: [], loading: false, status: ''}, action) =>{
+    switch(action.type){
+        case 'LOADING_PROJECTS':
+            return {
+                ...state,
+                projects: [...state.projects],
+                loading: true,
+                status: null
+            }
+        case 'ADD_PROJECTS':
+            return {
+                ...state,
+                projects: [action.projects],
+                loading: false
+            }
+        case 'PROJECTS_ERROR':
+            return {
+                ...state,
+                status: action.status
+            }
+        default:
+            return state
+    }
+}
