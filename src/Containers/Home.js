cfab2f6fbfebf268a01e2558e9dba9c3cc0d271a
dean <deanlloyd64@gmail.com>
2020-09-26 23:29:33 -0400

    add blog/project reducer and action files (blank)

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -8,20 +8,10 @@ class Home extends Component{
             <>
                 <Row className="header h-100">
                     <Col className="">
-                        <h1 className="text-center">Hi I'm Dean I'm a FullStack Engineer experienced in Ruby on Rails and JavaScript based programming with a background in React and Sinatra.</h1>
+                        <h1 className="text-center">
+                            Hi I'm Dean I'm a FullStack Engineer experienced in Ruby on Rails and JavaScript based programming with a background in React and Sinatra.
+                        </h1>
                     </Col>
-                    <Col>
-                    <Row className="mostRecent">
-                        <h3>Most Recent Project: title</h3>
-                        <p>Image or gif here</p>
-                        <p>Text that begins to fade out or some shit</p>
-                    </Row>
-                    <Row className="mostRecent">
-                        <h3>Most Recent Blog: title</h3>
-                        <p>Image or gif here</p>
-                        <p>Text that begins to fade out or some shit</p>
-                    </Row>
-                </Col>
                 </Row>
             </>
         )
