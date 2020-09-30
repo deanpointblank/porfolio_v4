044356db0387edc3402e7ddb0343a17b1265752b
dean <deanlloyd64@gmail.com>
2020-09-30 00:34:15 -0400

    home page structure more or up to par

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -7,13 +7,29 @@ class Home extends Component{
         return(
             <>
                 <Col fluid>
-                    <Row className="h-100">
-                        <Col className="d-flex align-items-center">
-                        <h1 className="text-center">
+                    <Row className="h-75 align-items-center">
+                        <Col className="text-center">
+                        <h1>
                             Hi I'm Dean I'm a FullStack Engineer experienced in Ruby on Rails and JavaScript based programming.
                         </h1>
+                        <br />
+                        <h4 cassName="lead">
+                            -I know other stuff too-
+                        </h4>
                         </Col>
                     </Row>
+                    <Row className="h-auto">
+                            <Col className="text-center home-links">
+                                <h3>
+                                    Who is Dean?
+                                </h3>
+                            </Col>
+                            <Col className="text-center home-links">
+                                <h3>
+                                    What does he know?
+                                </h3>
+                            </Col>
+                        </Row>
                 </Col>
             </>
         )
--- src/Containers/Nav.js
+++ src/Containers/Nav.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Col, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { GrLinkedin, GrGithub, GrTwitter, GrMail, GrMenu } from 'react-icons/gr'
+import { NavLink } from 'react-router-dom';
 
 
 export default class Menu extends Component {
@@ -22,16 +23,17 @@ export default class Menu extends Component {
         return(
             <>
                 <Navbar bg="light" expand="lg" fluid className="justify-content-between">
-                <Navbar.Brand href="/">Dean Betty</Navbar.Brand>
+                <NavLink className="navbar-brand" to="/">Dean Betty</NavLink>
                     <span onClick={event => this.toggleNav(event)}><GrMenu /></span>
                 </Navbar>
                 <div className={'full-screen flex-container-center ' + this.state.hidden}>
                     <span className="nav-close" onClick={event => this.toggleNav(event)}>X</span>
-                    <Nav defaultActiveKey="/home" className="flex-column">
-                        <Nav.Link href="/about">About</Nav.Link>
-                        <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-                        <Nav.Link href="/blog">Blog</Nav.Link>
-                        <Nav.Link href="#">Contact</Nav.Link>
+                    <Nav activeKey="/home" className="flex-column">
+                        <NavLink className="nav-link" to="/" activeClassName="selected">Home</NavLink>
+                        <NavLink className="nav-link" to="/about" activeClassName="selected">About</NavLink>
+                        <NavLink className="nav-link" to="/portfolio" activeClassName="selected">Portfolio</NavLink>
+                        <NavLink className="nav-link" to="/blog" activeClassName="selected">Blog</NavLink>
+                        <NavLink className="nav-link" to="#" activeClassName="selected">Contact</NavLink>
                         <Navbar.Text >
                         <a className="mr-2" href="https://github.com/deanpointblank"><GrGithub /></a> <a className="mr-2 ml-2" href="https://www.linkedin.com/in/deanbetty/"><GrLinkedin /></a> <a className="mr-2 ml-2" href="https://twitter.com/TheDomDonDada"><GrTwitter /></a><a className="mr-2 ml-2" href="#Email"><GrMail /></a>
                         </Navbar.Text>
