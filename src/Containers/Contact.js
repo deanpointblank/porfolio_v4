37ee523dd32fa4e774ce5c4e9fb6b4579cd326e9
dean <deanlloyd64@gmail.com>
2020-09-30 22:26:13 -0400

    create place holder text for contact container

---
+++ src/Containers/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Contact extends Component{
+    render(){
+        return(
+            <>
+                <h1>
+                    This is a contact page
+                </h1>
+            </>
+        )
+    }
+}
