e3c8e2b5e37f130e92b1f03dc3a238a32f13a474
dean <deanlloyd64@gmail.com>
2020-09-30 22:38:45 -0400

    added dummy form to contact page

--- src/Containers/Contact.js
+++ src/Containers/Contact.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 export default class Contact extends Component{
     render(){
         return(
             <>
-                <h1>
-                    This is a contact page
-                </h1>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Control type="text" placeholder="Normal text" />
+                    <br />
+
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Example textarea</Form.Label>
+                        <Form.Control as="textarea" rows="3" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
             </>
         )
     }
