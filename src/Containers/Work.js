878e549b9233fc5855463717b3587643a4e5dc28
dean <deanlloyd64@gmail.com>
2020-10-08 23:14:19 -0400

    fil in template for the about/work page

---
+++ src/Containers/Work.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Col, Row} from 'react-bootstrap';
+
+export default class Work extends Component{
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <>
+
+            </>
+        )
+    }
+}
