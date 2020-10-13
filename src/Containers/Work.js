7f65ba170887cf001b46f276cef80c1426c5f63d
dean <deanlloyd64@gmail.com>
2020-10-12 20:14:40 -0400

    partitioned work.js into 4 sections

--- src/Containers/Work.js
+++ src/Containers/Work.js
@@ -12,23 +12,31 @@ export default class Work extends Component{
     render(){
         return(
             <>
-            <Col>
-                <Row>
+                <Col fluid>
+                    <Row className="h-50">
+                        <Col className="h-100">
+                            <div className="Menu">
+                                <div className="center">Menu
+                                    <div className="button">1</div>
+                                    <div className="button">2</div>
+                                    <div className="button">3</div>
+                                    <div className="button">4</div>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col className="h-100">
 
-                </Row>
-                <Row>
-                    
-                </Row>
-            </Col>
-            <Col>
-                <Row>
+                        </Col>
+                    </Row>
+                    <Row className="h-50">
+                        <Col className="h-100">
 
-                </Row>
-                <Row>
-                    
-                </Row>
-            </Col>
+                        </Col>
+                        <Col className="h-100">
 
+                        </Col>
+                    </Row>
+                </Col>
             </>
         )
     }
