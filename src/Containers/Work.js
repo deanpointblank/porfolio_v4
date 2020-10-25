dd75195d75a3a6d85294b7e0c4ecf313ea54b656
dean <deanlloyd64@gmail.com>
2020-10-25 19:17:48 -0400

    update

--- src/Containers/Work.js
+++ src/Containers/Work.js
@@ -15,25 +15,14 @@ export default class Work extends Component{
                 <Col fluid>
                     <Row className="h-50">
                         <Col className="h-100">
-                            <div className="Menu">
-                                <div className="center">Menu
-                                    <div className="button">1</div>
-                                    <div className="button">2</div>
-                                    <div className="button">3</div>
-                                    <div className="button">4</div>
-                                </div>
-                            </div>
                         </Col>
                         <Col className="h-100">
-
                         </Col>
                     </Row>
                     <Row className="h-50">
                         <Col className="h-100">
-
                         </Col>
                         <Col className="h-100">
-
                         </Col>
                     </Row>
                 </Col>
